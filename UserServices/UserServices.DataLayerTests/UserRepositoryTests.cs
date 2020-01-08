75409f450d65dd0cae320b6d96cf66f92dee37e3
Thomas-Lion <thmslion@gmail.com>
2020-01-08 14:25:22 +0100

    For Max

--- UserServices/UserServices.DataLayerTests/UserRepositoryTests.cs
+++ UserServices/UserServices.DataLayerTests/UserRepositoryTests.cs
@@ -1,15 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using UserServices.DataLayer;
+using UserServices.Shared.TransferObject;
 
 namespace UserServices.DataLayerTests
 {
     [TestClass]
     public class UserRepositoryTest
     {
-        [TestMethod()]
-        public void UserRepositoryInsertInDB_WhenValid()
-        {
-         //   var options = new DbContextOptionsBuilder<RegistrationServicesContext>();
+        //[TestMethod()]
+        //public void UserRepositoryInsertInDB_WhenValid()
+        //{
+        //    var options = new DbContextOptionsBuilder<RegistrationServicesContext>()
+        //        .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
+        //        .Options;
+
+        //    using (var RSCxt = new RegistrationServicesContext(options))
+        //    {
+        //        //Arrange
+        //        var userToUse = new UserTO() 
+        //        { 
+        //            ID
+        //        };
+        //        //Act
+
+                //Assert
+            }
         }
     }
 }
